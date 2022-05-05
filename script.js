33329a57879d3fd6f99402cf204c80ffe5890b34
arthur <arthurklo@msn.com>
2022-05-05 16:31:25 +0200

    essai sous menu 1

--- script.js
+++ script.js
@@ -1,8 +1,76 @@
 
-var animal = [
-    { image: "truc" },
-    { nom: "fnec" },
-    { age: "22" },
-    { description: "blablabla" },
-    { prix: 500 }
+var animalerie = [
+    {
+        name: "Felins",
+        ref: "felins",
+        especes: [
+            {
+                name: "Chats",
+                ref: "chats",
+                liste: [
+                    { image: "truc", nom: "fnec", age: "2", description: "blablablsda", prix: 5040 },
+                    { image: "trucA", nom: "fnac", age: "1", description: "blagblasbla", prix: 3500 },
+                    { image: "trucB", nom: "fnuc", age: "3", description: "blabtrhlabla", prix: 5020 },
+                    { image: "trucC", nom: "fnoc", age: "2", description: "blabl,,habla", prix: 5001 }
+                ]
+            },
+            {
+                name: "Lions",
+                ref: "lions",
+                liste: [
+                    { image: "truc", nom: "fnec", age: "2", description: "blablablsda", prix: 5040 },
+                    { image: "trucA", nom: "fnac", age: "1", description: "blagblasbla", prix: 3500 },
+                    { image: "trucB", nom: "fnuc", age: "3", description: "blabtrhlabla", prix: 5020 },
+                    { image: "trucC", nom: "fnoc", age: "2", description: "blabl,,habla", prix: 5001 }
+                ]
+            },
+            {
+                name: "Chats",
+                ref: "chats",
+                liste: [
+                    { image: "truc", nom: "fnec", age: "2", description: "blablablsda", prix: 5040 },
+                    { image: "trucA", nom: "fnac", age: "1", description: "blagblasbla", prix: 3500 },
+                    { image: "trucB", nom: "fnuc", age: "3", description: "blabtrhlabla", prix: 5020 },
+                    { image: "trucC", nom: "fnoc", age: "2", description: "blabl,,habla", prix: 5001 }
+                ]
+            }
+        ]
+    },
+    {
+        name: "Canins",
+        ref: "canins",
+        especes: [
+            {
+                name: "Chiens",
+                ref: "chiens",
+                liste: [
+                    { image: "truc", nom: "fnec", age: "2", description: "blablablsda", prix: 5040 },
+                    { image: "trucA", nom: "fnac", age: "1", description: "blagblasbla", prix: 3500 },
+                    { image: "trucB", nom: "fnuc", age: "3", description: "blabtrhlabla", prix: 5020 },
+                    { image: "trucC", nom: "fnoc", age: "2", description: "blabl,,habla", prix: 5001 }
+                ]
+            },
+            {
+                name: "Loups",
+                ref: "loups",
+                liste: [
+                    { image: "truc", nom: "fnec", age: "2", description: "blablablsda", prix: 5040 },
+                    { image: "trucA", nom: "fnac", age: "1", description: "blagblasbla", prix: 3500 },
+                    { image: "trucB", nom: "fnuc", age: "3", description: "blabtrhlabla", prix: 5020 },
+                    { image: "trucC", nom: "fnoc", age: "2", description: "blabl,,habla", prix: 5001 }
+                ]
+            },
+            {
+                name: "Renards",
+                ref: "renards",
+                liste: [
+                    { image: "truc", nom: "fnec", age: "2", description: "blablablsda", prix: 5040 },
+                    { image: "trucA", nom: "fnac", age: "1", description: "blagblasbla", prix: 3500 },
+                    { image: "trucB", nom: "fnuc", age: "3", description: "blabtrhlabla", prix: 5020 },
+                    { image: "trucC", nom: "fnoc", age: "2", description: "blabl,,habla", prix: 5001 }
+                ]
+            }
+        ]
+    }
 ]
+
