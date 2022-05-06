394a86b30ccfface7c2427a829f6c57d34661c5e
arthur <arthurklo@msn.com>
2022-05-06 12:35:07 +0200

    plus qu'a boucler sur le json !

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
-var animalerie = [
+
+let animalerie = [
     {
         name: "Felins",
         ref: "felins",
@@ -75,23 +76,110 @@ var animalerie = [
 ]
 
 
+function AddEspeceInFamille() {
+    var newSubMenu = document.createElement('ul');
+    newSubMenu.classList.add('dropdown-menu');
 
-/*
-    < div class= "col d-flex nav navbar-nav" >
+    var newSubMenuList = document.createElement('li');
+    newSubMenuList.classList.add('dropright-submenu');
 
-    <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-            aria-haspopup="true" aria-expanded="true"> <span class="nav-label">Felins</span>
-            <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-            <li class="dropright-submenu">
-                <a href="#" class="dropright-toggle" data-toggle="dropdown" role="button"
-                    aria-haspopup="true" aria-expanded="false"> <span class="nav-label"
-                        id="espece">Chatons</span><span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="#">Chaton1</a></li>
-                    <li><a href="#">Chaton2</a></li>
-                    <li><a href="#">Chaton3</a></li>
-                </ul>
-            </li>
-*/
+    var newSubMenuButton = document.createElement('a');
+    newSubMenuButton.href = "#"
+    newSubMenuButton.classList.add('dropright-toggle');
+    newSubMenuButton.dataToggle = "dropdown"
+    newSubMenuButton.role = "button"
+    newSubMenuButton.ariaHasPopup = "true"
+    newSubMenuButton.ariaExpanded = "false"
+
+    var SubMenuName = document.createElement('span');
+    SubMenuName.classList.add('nav-label');
+    ////////////////////////cherher le nom
+    SubMenuName.id = "espece1"
+    ////////////////////////
+    SubMenuName.setAttribute("data-espece", "espece")
+    SubMenuName.innerHTML = "espece"
+
+    var newSubSubMenu = document.createElement('ul');
+    newSubSubMenu.classList.add('dropdown-menu');
+
+    //////////////////////////////dans boucle for
+    var newBaby = document.createElement('li');
+    var newBabyLink = document.createElement('a');
+    newBabyLink.href = "#"
+    newBabyLink.innerHTML = "testFinal1"
+
+    var newBaby2 = document.createElement('li');
+    var newBabyLink2 = document.createElement('a');
+    newBabyLink2.href = "#"
+    newBabyLink2.innerHTML = "testFinal2"
+    ////////////////////////
+
+    newBaby2.appendChild(newBabyLink2)
+    newSubSubMenu.appendChild(newBaby2)
+    newBaby.appendChild(newBabyLink)
+    newSubSubMenu.appendChild(newBaby)
+
+
+    newSubMenuButton.appendChild(SubMenuName)
+    newSubMenuList.appendChild(newSubMenuButton)
+    newSubMenuList.appendChild(newSubSubMenu)
+    newSubMenu.appendChild(newSubMenuList)
+
+    return newSubMenu;
+
+}
+
+function AddFamilleInCol() {
+
+    var newDropdownMenu = document.createElement('ul');
+    newDropdownMenu.classList.add('dropdown');
+
+    var famille = document.createElement('a');
+    famille.classList.add('dropdown-toggle');
+    famille.setAttribute("data-toggle", "dropdown");
+
+    famille.ariaRoleDescription = 'button'
+    famille.ariaHasPopup = 'true'
+    famille.ariaExpanded = 'true'
+
+    var nomFamille = document.createElement('span');
+    nomFamille.classList.add('nav-label')
+    ////////////////////////cherher le nom
+    nomFamille.id = "famille1"
+    ////////////////////////
+    nomFamille.setAttribute("data-famille", "famille")
+    nomFamille.innerHTML = "Famille"
+
+    famille.appendChild(nomFamille)
+    newDropdownMenu.appendChild(famille)
+    return newDropdownMenu;
+}
+
+function AddColMenu() {
+    var newColMenu = document.createElement('div');
+    newColMenu.classList.add('col');
+    newColMenu.classList.add('d-flex');
+    newColMenu.classList.add('nav');
+    newColMenu.classList.add('navbar-nav');
+
+    return newColMenu
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    var menu = document.querySelector('#menu')
+    var col = AddColMenu();
+
+    animalerie.forEach(element => {
+
+        console.log(element.name)
+
+    });
+    var famille = AddFamilleInCol();
+    var espece = AddEspeceInFamille();
+
+
+    famille.appendChild(espece);
+    col.appendChild(famille);
+    menu.appendChild(col);
+
+});
