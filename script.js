51e7f7aeb10582c8c1e1bce6b8704e535db87345
arthur <arthurklo@msn.com>
2022-05-06 10:09:29 +0200

    squelette moche mais fini (sauf panier)

--- script.js
+++ script.js
@@ -74,3 +74,24 @@ var animalerie = [
     }
 ]
 
+
+
+/*
+    < div class= "col d-flex nav navbar-nav" >
+
+    <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+            aria-haspopup="true" aria-expanded="true"> <span class="nav-label">Felins</span>
+            <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+            <li class="dropright-submenu">
+                <a href="#" class="dropright-toggle" data-toggle="dropdown" role="button"
+                    aria-haspopup="true" aria-expanded="false"> <span class="nav-label"
+                        id="espece">Chatons</span><span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Chaton1</a></li>
+                    <li><a href="#">Chaton2</a></li>
+                    <li><a href="#">Chaton3</a></li>
+                </ul>
+            </li>
+*/
